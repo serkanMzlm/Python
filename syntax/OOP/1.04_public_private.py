043cc8664bc84d25e8b2fb764eb87641eae7bc04
serkanMzlm <74418302+serkanMzlm@users.noreply.github.com>
2021-03-28 15:01:15 +0300

    Update 1.04_public_private.py

--- syntax/OOP/1.04_public_private.py
+++ syntax/OOP/1.04_public_private.py
@@ -5,4 +5,4 @@ class sinif:
         self.__deger1.append(deger2)
 o1 = sinif("ali")
 print(o1.deger2) #__deger1 private oldugu icin erişimimiz yok deger2 public oldugu için her yerden erişebiliriz
-print(o1._sinif__deger1) #private dısardan erişmek için biryöntem
+print(o1._sinif__deger1) #private dısardan erişmek için bir yöntem nesne ismi _ sinif ismi ve gizli metot
